94cdd719180999f6ea99ae1510b49260b59ba9ca
Neethu M <128774398+Neethu-Muthu@users.noreply.github.com>
2024-09-05 22:30:00 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <div className="mx-auto w-full text-center">
-      <p className="text-xl">Made with ❤️ by <a className="hover:text-gray-400" href="https://www.linkedin.com/in/neethumuthu/" target="_blank" rel="noreferrer"> Neethu M </a></p>
+      <p className="text-xl"><a className="hover:text-gray-400" href="https://www.linkedin.com/in/neethumuthu/" target="_blank" rel="noreferrer"></a></p>
     </div>
   )
 }
